ad9dc3cff290439bed425d39f26445b252af1eff
Rodrigo Gamboa Loera <RodrigoGamboaLoera@hotmail.com>
2021-06-10 13:55:52 -0600

    3commit

--- Program.cs
+++ Program.cs
@@ -6,9 +6,31 @@ namespace Stack
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Introduce tu nombre:");
-            string nombre = Console.ReadLine();
-            Console.WriteLine("Hola " + nombre );
+            // 1) Decir "hola mundo"
+            // 2) Decir "hola [nombre]!"
+            // 9) salir
+
+            // while
+
+            // Mostrar menu de opciones
+            // Solicitar la opcion al usuario
+            Console.WriteLine("1) Decir \"Hola Mundo!\"");
+            Console.WriteLine("2) Decir \"Hola [nombre]!\"");
+            Console.WriteLine("9) Salir ");
+
+            Console.WriteLine("Por favor selecciona una opciòn:");
+            string opcion = Console.ReadLine();
+            if (opcion == "1")
+            {
+                Console.WriteLine("Hola Mundo!");
+            }
+            else if (opcion == "2")
+            {
+                Console.WriteLine("Introduce tu nombre: ");
+                string nombre = Console.ReadLine();
+                Console.WriteLine("Hola " + nombre + "!");
+            }
+
         }
     }
 }
