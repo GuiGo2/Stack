969b533efd999d1e7c55db3b247aebc2e4cee8d8
Rodrigo Gamboa Loera <RodrigoGamboaLoera@hotmail.com>
2021-06-10 14:45:06 -0600

    4commit

--- Program.cs
+++ Program.cs
@@ -10,41 +10,55 @@ namespace Stack
             // 2) Decir "hola [nombre]!"
             // 9) salir
             string opcion = "";
-            while(opcion != "1" && opcion != "2" && opcion != "9")
-            {
-
-                // Mostrar menu de opciones
-                // Solicitar la opcion al usuario
-                Console.WriteLine("1) Decir \"Hola Mundo!\"");
-                Console.WriteLine("2) Decir \"Hola [nombre]!\"");
-                Console.WriteLine("9) Salir ");
-
-
-
-                Console.WriteLine("Por favor selecciona una opciòn:");
-                opcion = Console.ReadLine();
-
-                            // Porfavor seleccione una opcion valida hasta que seinstruzca una opcion correcta
-                            //mientras no se haya introducido una oopcion correcta
-                            if (opcion != "1" && opcion != "2" && opcion != "9")
-                            {
-                                Console.WriteLine("La opcion sleccionada no es correcta");
-
-                            }
-
-
-                if (opcion == "1")
+            while (opcion != "9")
+            {   
+                opcion = "";
+                // pedir opcion al usuario
+                while(opcion != "1" && opcion != "2" && opcion != "9")
                 {
-                    Console.WriteLine("Hola Mundo!");
-                }
-                else if (opcion == "2")
-                {
-                    Console.WriteLine("Introduce tu nombre: ");
-                    string nombre = Console.ReadLine();
-                    Console.WriteLine("Hola " + nombre + "!");
-                }
+                    
+                    // Mostrar menu de opciones
+                    // Solicitar la opcion al usuario
+                    Console.WriteLine("1) Decir \"Hola Mundo!\"");
+                    Console.WriteLine("2) Decir \"Hola [nombre]!\"");
+                    Console.WriteLine("9) Salir ");
 
+
+
+                    Console.WriteLine("Por favor selecciona una opciòn:");
+                    opcion = Console.ReadLine();
+
+                                // Porfavor seleccione una opcion valida hasta que seinstruzca una opcion correcta
+                                //mientras no se haya introducido una oopcion correcta
+                                if (opcion != "1" && opcion != "2" && opcion != "9")
+                                {
+                                    Console.WriteLine("La opcion sleccionada no es correcta");
+
+                                }
+
+
+                    if (opcion == "1")
+                    {
+                        Console.WriteLine("Hola Mundo!");
+                    }
+                    else if (opcion == "2")
+                    {
+                        Console.WriteLine("Introduce tu nombre: ");
+                        string nombre = Console.ReadLine();
+                        Console.WriteLine("Hola " + nombre + "!");
+                    }
+                    else if (opcion == "9")
+                    {
+                        Console.WriteLine("adios");
+                    }
+                    
+                        
+                    
+
+                }
+                
             }
+            
 
         }
     }
